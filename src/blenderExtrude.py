eead0839b08f42d50d195330aa6866e9e00e55e5
spresse1 <steve@pressers.name>
2023-02-19 09:27:07 -0500

    Convert all objects to meshes

--- src/blenderExtrude.py
+++ src/blenderExtrude.py
@@ -4,15 +4,19 @@ bpy.context.scene.unit_settings.length_unit = 'MILLIMETERS'
 
 bpy.ops.import_curve.svg (filepath=bpy.path.abspath("//test_map.svg"))
 
+bpy.ops.object.select_all(action='DESELECT')
+
 for obj in bpy.data.collections[1].all_objects:
-    #print(obj)
-    try:
-        obj.data.extrude = 0.0004 + 0.001 * obj.data.materials[0].diffuse_color[0]
-        obj.location[2] = obj.data.extrude * 100 #0.04 + 0.01 * obj.data.materials[0].diffuse_color[0]
-    except:
-        pass
+    if obj.type == 'CURVE':
+        if obj.data.materials[0]:
+            obj.data.extrude = 0.0004 + 0.001 * obj.data.materials[0].diffuse_color[0]
+            obj.location[2] = obj.data.extrude * 100 #0.04 + 0.01 * obj.data.materials[0].diffuse_color[0]
+        bpy.context.view_layer.objects.active = obj
+        obj.select_set(True)
+        bpy.ops.object.convert(target='MESH')
+        obj.select_set(False)
     obj.scale[0] = 100
     obj.scale[1] = 100
     obj.scale[2] = 100
-    
+
 bpy.ops.export_mesh.stl(filepath=bpy.path.abspath("//test_map.stl"), check_existing=False)
